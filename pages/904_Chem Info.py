e89469e2368ff5b7f5bfe7ac25adec279be2ae4c
ryan mellor <ryan.mellor.16@ucl.ac.uk>
2024-07-17 16:51:14 +0100

    Updated ChemInfo

--- pages/904_Chem Info.py
+++ pages/904_Chem Info.py
@@ -17,12 +17,23 @@ import pubchempy as pcp
 warnings.filterwarnings('ignore')
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-from helpers import sci_setup, sci_data, sci_safety
+from helpers import sci_setup, sci_data
 from helpers.sci_style import *
 sci_setup.setup_page("Chem Info")
 
 HAZARD_STATEMENTS = pd.read_excel("./assets/public_data/ghs_h_codes.xlsx")
 PRECAUTIONARY_STATEMENTS = pd.read_excel("./assets/public_data/ghs_p_codes.xlsx")
+PICTOGRAMS = {
+    "GHS01": {"name": "Explosive", "image": "./assets/images/GHS01.png"},
+    "GHS02": {"name": "Flammable", "image": "./assets/images/GHS02.png"},
+    "GHS03": {"name": "Oxidizer", "image": "./assets/images/GHS03.png"},
+    "GHS04": {"name": "Compressed Gas", "image": "./assets/images/GHS04.png"},
+    "GHS05": {"name": "Corrosive", "image": "./assets/images/GHS05.png"},
+    "GHS06": {"name": "Acute Toxic", "image": "./assets/images/GHS06.png"},
+    "GHS07": {"name": "Irritant", "image": "./assets/images/GHS07.png"},
+    "GHS08": {"name": "Health Hazard", "image": "./assets/images/GHS08.png"},
+    "GHS09": {"name": "Environment", "image": "./assets/images/GHS09.png"},
+}
 
 re_cas = re.compile(r'\d{2,7}-\d\d-\d')
 re_ghs_p_statements = re.compile(r'(P\d{3})')
